7a58d2d867fe90bd2a5dc5f520ca58a14ae30007
Panda, Asish <asish312@gmail.com>
2020-03-02 00:07:01 +0530

    Updated Asish

--- Program.cs
+++ Program.cs
@@ -8,11 +8,56 @@ namespace ConsoleApps
 {
     class Program
     {
+        public Program()
+        {
+            Console.WriteLine("This is Default Parent Constructor has called in Program");
+        }
+
+        public void Method1()
+        {
+            Console.WriteLine("This is Default Parent Method has called in Program");
+        }
+    }
+    class ChildProgram : Program
+    {
+        public ChildProgram()
+        {
+            Console.WriteLine("This is Child Constructor has called in Program");
+        }
+
+        //public override void Method1()
+        //{
+        //    Console.WriteLine("This is Child Method has called in Program");
+        //}
         static void Main(string[] args)
         {
-            Console.WriteLine("~!Welcome 2 my World!~");
+            //ChildProgram C = new ChildProgram();
+            //C.Method1();
+
+
+            //Program P = new Program();
+            //P.Method1();
+
+            //Program C = new ChildProgram();
+            //C.Method1();
+
+            //ChildProgram C = new ChildProgram();
+            //Program P=C;
+            //P.Method1();    //Default Parent Method
+
+
+            //Program C = new Program();
+            //ChildProgram P =(ChildProgram) C;    //run time error
+            //P.Method1(); 
+
+            //Program C = new ChildProgram();
+            //C.Method1();
+
+            ChildProgram C = new ChildProgram();
+            C.Method1();
 
             Console.ReadLine();
+
         }
     }
 }
